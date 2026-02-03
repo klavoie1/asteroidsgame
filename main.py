d27eea2275ff66e053a6775f526f5bbbb7fdd770
klavoie1 <kenlavoie19@gmail.com>
2026-02-03 18:22:15 -0500

    added window functionality

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+def main():
+
+    running = True
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while (running == True):
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+
+    
+
+
+if __name__ == "__main__":
+    main()
